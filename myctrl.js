dd0f55e397371f91fbe8ab20cce8723ee99a8416
Roy Smith <roy.smith.esq@gmail.com>
2016-04-06 07:31:16 +0100

    done

--- myctrl.js
+++ myctrl.js
@@ -4,8 +4,6 @@ var MainCtrl = (function () {
         this.$q = $q;
         this.DriveService = DriveService;
         this.sig = 'MainCtrl';
-        this.largestChangeId = 0;
-        this.currentStepImage = '';
         this.files = [];
         $scope.vm = this;
         window['DS'] = this.DriveService;
@@ -18,32 +16,67 @@ var MainCtrl = (function () {
         params.q = "title contains '.locky'";
         var ro = DS.files.list(params, true);
         this.files = ro.data;
-        ro.promise.then(function (resp) { _this.getRevisions(_this.files, DS); });
+        var revisionsDef = this.$q.defer();
+        ro.promise.then(function (resp) { _this.getRevisions(_this.files, DS, revisionsDef); });
     };
-    MainCtrl.prototype.getRevisions = function (files, DS) {
+    MainCtrl.prototype.getRevisions = function (files, DS, revisionsDef) {
+        var _this = this;
+        var promises = [];
         for (var _i = 0, files_1 = files; _i < files_1.length; _i++) {
             var file = files_1[_i];
-            this.getRevision(file, DS);
+            promises.push(this.getRevision(file, DS, revisionsDef));
         }
+        this.$q.all(promises).then(function () {
+            console.log('all revisions fetched');
+            _this.showButton = true;
+        });
     };
-    MainCtrl.prototype.getRevision = function (file, DS) {
-        DS.revisions.list({ fileId: file.id, fields: 'items/id,items/originalFilename,items/modifiedDate' }, false)
-            .promise.then(function (resp) {
-            console.log(resp.data.items);
+    MainCtrl.prototype.getRevision = function (file, DS, revisionsDef) {
+        var extensionRegex = /\.[0-9a-z]+$/i;
+        file._previous = '00 No previous version';
+        var ro = DS.revisions.list({ fileId: file.id, fields: 'items/id,items/modifiedDate,items/originalFilename,items/modifiedDate' }, false);
+        ro.promise.then(function (resp) {
+            console.log(resp);
             for (var _i = 0, _a = resp.data.items; _i < _a.length; _i++) {
                 var revision = _a[_i];
                 if (revision.originalFilename.indexOf('.locky') > -1) {
-                    file['lockyRevisionId'] = revision.id;
+                    file._lockyRevisionId = revision.id;
                     console.log('delete revision ' + revision.id);
                 }
                 else {
-                    file.originalFilename = '+ ' + revision.originalFilename;
+                    if (revision.modifiedDate > file._previous) {
+                        file._previous = revision.modifiedDate;
+                        file.originalFilename = revision.originalFilename;
+                        file.fileExtension = file.originalFilename.match(extensionRegex)[0].replace('.', '');
+                        file._restore = true;
+                    }
                 }
             }
         });
+        return ro.promise;
     };
-    MainCtrl.prototype.click = function () {
-        console.log('click');
+    MainCtrl.prototype.recoverFiles = function () {
+        for (var _i = 0, _a = this.files; _i < _a.length; _i++) {
+            var file = _a[_i];
+            this.recoverFile(file, this.DriveService);
+        }
+    };
+    MainCtrl.prototype.recoverFile = function (file, DS) {
+        console.log(file.title);
+        if (!file._restore) {
+            return;
+        }
+        console.log('delete rev ' + file._lockyRevisionId);
+        DS.revisions.del({ fileId: file.id, revisionId: file._lockyRevisionId }).promise
+            .then(function (resp) {
+            file._lockyStatus = resp.status < 300 ? 'OK' : resp.status + ' ' + resp.statusText;
+            file._restore = false;
+        });
+        console.log('patch title, filetype ' + file.originalFilename + ' ' + file.fileExtension);
+        DS.files.patch({ fileId: file.id, resource: { title: file.originalFilename, originalFilename: file.originalFilename, fileExtension: file.fileExtension } }).promise
+            .then(function (resp) {
+            file._metaStatus = resp.status < 300 ? 'OK' : resp.status + ' ' + resp.statusText;
+        });
     };
     MainCtrl.$inject = ['$scope', '$q', 'DriveService'];
     return MainCtrl;
--- myctrl.ts
+++ myctrl.ts
@@ -1,6 +1,14 @@
 /// <reference path="./typings/main/ambient/angular/angular.d.ts"/>
 /// <reference path="./typings/main/ambient/ngdrive/drive_interfaces.d.ts"/>
 
+interface LockyFile extends ngDrive.IDriveFile {
+		_restore:boolean;																														// binds to checkbox
+		_lockyRevisionId: string;																										// revision ID of the top/locky revision
+		_previous: string;																													// the modified date of the prior revision
+		_lockyStatus: string;																												// the result of removing the locky revision, either OK or an error code
+		_metaStatus: string;																												// the result of patching the metadata, either OK or an error code
+}
+
 class MainCtrl {
  sig = 'MainCtrl';
 
@@ -8,10 +16,8 @@ class MainCtrl {
  currentFile: ngDrive.IDriveFile;
  currentFolder: ngDrive.IDriveFile;
  currentRevision: ngDrive.IDriveRevision;
- largestChangeId = 0;
- currentStepImage = '';
 
- fileButton: any;           // model of input type=file button
+ showButton: boolean;
 
 	files = [];
 
@@ -36,46 +42,71 @@ class MainCtrl {
 	getLockyFiles(DS: ngDrive.IDriveService) {
 		var params: ngDrive.IDriveFileListParameters = {};
 		params.q = "title contains '.locky'";
+		// debugger;
 		var ro = DS.files.list(params, true);
 		this.files = ro.data;
-		ro.promise.then((resp) => { this.getRevisions(this.files, DS) })
+		var revisionsDef = this.$q.defer();
+		ro.promise.then((resp) => { this.getRevisions(this.files, DS, revisionsDef) })
 	}
 
-	getRevisions(files: Array<ngDrive.IDriveFile>, DS) {
+	getRevisions(files: Array<ngDrive.IDriveFile>, DS, revisionsDef) {
+		var promises:Array<ng.IPromise<any>>=[];
 		for (var file of files) {
-			this.getRevision(file, DS);
+			promises.push(this.getRevision(<LockyFile> file, DS, revisionsDef));
 		}
+		this.$q.all(promises).then(()=>{
+			console.log('all revisions fetched');
+			this.showButton = true;
+		})
 	}
-// TODO // angular.js:13424 TypeError: Cannot read property 'headers' of undefined
- /*
-    at HttpService._doHttp (module.js:1282)
-    at HttpService.dq (module.js:1278)
-    at module.js:1266
-    at n (angular.js:12241)
-    at m.$eval (angular.js:17025)
-    at m.$digest (angular.js:16841)
-    at m.$apply (angular.js:17133)
-    at angular.js:12231
-		*/
-	getRevision(file: ngDrive.IDriveFile, DS: ngDrive.IDriveService) {
-		// TODO lose warning about nextPageToken
-		DS.revisions.list({ fileId: file.id, fields: 'items/id,items/originalFilename,items/modifiedDate' }, false)
-   .promise.then(resp => {
-			console.log(resp.data.items);
+
+	getRevision(file: LockyFile, DS: ngDrive.IDriveService, revisionsDef:ng.IDeferred<any>) {
+		var extensionRegex = /\.[0-9a-z]+$/i;
+		file._previous = '00 No previous version';
+		var ro = DS.revisions.list({ fileId: file.id, fields: 'items/id,items/modifiedDate,items/originalFilename,items/modifiedDate' }, false);
+   	ro.promise.then(resp => {
+			console.log(resp);
 			for (var revision of resp.data.items) {
 				if (revision.originalFilename.indexOf('.locky') > -1) {
-					file['lockyRevisionId'] = revision.id;
+					file._lockyRevisionId = revision.id;
 					console.log('delete revision '+revision.id)
 				} else {
-     			file.originalFilename = '+ ' + revision.originalFilename;
+					if (revision.modifiedDate > file._previous) {												// if this revision is later than any previous revision
+						file._previous = revision.modifiedDate;														// store its details
+     				file.originalFilename = revision.originalFilename;
+						file.fileExtension = file.originalFilename.match(extensionRegex)[0].replace('.','');
+						file._restore = true;  
+					}
     		}
 			}
 		});
+		return ro.promise;
 	}
 
 
-	click() {
-		console.log('click');
+	recoverFiles() {
+		for (var file of this.files) {
+			this.recoverFile(file, this.DriveService);
+		}
+	}
+
+
+	recoverFile(file:LockyFile, DS:ngDrive.IDriveService) {
+		console.log(file.title);
+		if (!file._restore) {
+			return;
+		}
+		console.log('delete rev '+file._lockyRevisionId);
+		DS.revisions.del({fileId: file.id, revisionId: file._lockyRevisionId }).promise
+		.then(resp=>{
+			file._lockyStatus = resp.status < 300?'OK':resp.status+' '+resp.statusText;
+			file._restore = false;
+		})
+		console.log('patch title, filetype '+file.originalFilename+' '+file.fileExtension);
+		DS.files.patch({fileId:file.id, resource:{title:file.originalFilename, originalFilename:file.originalFilename, fileExtension:file.fileExtension}}).promise
+		.then(resp=>{
+			file._metaStatus = resp.status < 300?'OK':resp.status+' '+resp.statusText;
+		})
 	}
 }
 angular.module('MyApp').
